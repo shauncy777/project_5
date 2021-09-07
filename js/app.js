d09b618eb947d9d06301992d8522503d7af1992a
shauncy777 <75694493+shauncy777@users.noreply.github.com>
2021-09-07 14:59:20 -0700

    debugging modal toggle

--- js/app.js
+++ js/app.js
@@ -1,7 +1,14 @@
 
-
+let index = 0;
+let profileData;
 const profileContainer = document.getElementById('gallery');
 const searchDiv = document.querySelector('.search-container');
+const body = document.querySelector('body');
+
+
+            
+body.style.backgroundColor="gray";
+
 
 
 // Insert search bar
@@ -12,13 +19,17 @@ const searchBar = `<form action="#" method="get">
     searchDiv.insertAdjacentHTML('beforeend', searchBar);
 
 
-// Single fetch request for 12 random users
-fetch('https://randomuser.me/api/?results=12')
+// Single fetch request for 12 random users from US which return proper formatted phone
+fetch('https://randomuser.me/api/?results=12&nat=us')
     .then( res => res.json())
     .then(data => {
-        generateGalleryHTML(data.results); 
+        profileData = data.results;
+        generateGalleryHTML(profileData);
+        eachModal(profileData);
     })
-
+    .catch(err => console.error('There was a problem loading the page')) 
+    
+   
 // Inserts each user into DOM
 function generateGalleryHTML(data) {
     for(let i = 0; i < data.length; i++){
@@ -34,4 +45,90 @@ function generateGalleryHTML(data) {
         </div>`;
     profileContainer.insertAdjacentHTML('beforeend', eachProfile);
     }
-}   
+}
+
+
+// Listens for click on profile card to show detailed modal profile card
+function eachModal (data){
+    const cardDiv = document.getElementsByClassName('card');
+        for(let i = 0; i < data.length; i++){
+            cardDiv[i].addEventListener('click', () => {        
+                generateModalCard(data[i]);   
+        });
+    }
+}
+
+// Dynamic markup for modal cards
+function generateModalCard(profile){
+       let modalCard = `<div class="modal-container">
+                            <div class="modal">
+                                <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+                                <div class="modal-info-container">
+                                    <img class="modal-img" src="${profile.picture.large}" alt="profile picture">
+                                    <h3 id="name" class="modal-name cap">${profile.name.first} ${profile.name.last}</h3>
+                                    <p class="modal-text">${profile.email}</p>
+                                    <p class="modal-text cap">${profile.location.city}</p>
+                                    <hr>
+                                    <p class="modal-text">${profile.phone}</p>
+                                    <p class="modal-text">${profile.location.street.number} ${profile.location.street.name}, ${profile.location.city}, ${profile.location.state} ${profile.location.postcode}</p>
+                                    <p class="modal-text">Birthday: ${profile.dob.date.slice(5,7)}/${profile.dob.date.slice(8,10)}/${profile.dob.date.slice(0,4)}</p>
+                                </div>
+                            </div>
+                            <div class="modal-btn-container">
+                            <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+                            <button type="button" id="modal-next" class="modal-next btn">Next</button>
+                        </div>
+                    </div>`;
+
+        profileContainer.insertAdjacentHTML('beforeend', modalCard);
+
+        // Variables for modal functionality 
+        const modalContainer = document.querySelector('.modal-container');
+        const modalClose = document.querySelector('.modal-close-btn');
+        
+            // Listens for click on profile card to close 
+            modalClose.addEventListener('click',() => {
+                modalContainer.remove();
+            });
+
+        const modalButt = document.querySelector('.modal-btn-container'); 
+        const prevButt = document.querySelector('#modal-prev');
+        const nextButt = document.querySelector('#modal-next')    
+        const profiles = document.querySelectorAll('.card');
+        //const profileArray = [...profiles];
+        //console.log(profileArray);
+        //console.log(prevButt);
+
+        modalButt.addEventListener('click', (e) => {
+
+            if (e.target === nextButt && index < profiles.length -1 ){
+                index ++;
+            } else if (e.target === nextButt && index == profiles.length -1){
+                index = 0;
+            } else if (e.target === prevButt && index > 0){
+                index --;
+            } else if (e.target === prevButt && index == 0){
+                index = profiles.length -1;
+            }
+                profileContainer.removeChild(modalContainer);
+                generateModalCard(profileData[index]); 
+            
+        });
+
+    }
+
+// This gives the search bar functionality
+searchDiv.addEventListener('keyup',(e) => {
+    let typedInput = e.target.value.toLowerCase();
+    const profileCard = document.querySelectorAll('.card');
+        for (let i = 0; i < profileCard.length; i ++){
+            let nameText = profileCard[i].querySelector('h3');
+            let profileName = nameText.textContent.toLowerCase();
+            if(profileName.includes(typedInput)){
+                profileCard[i].style.display = '';
+            } else {
+                profileCard[i].style.display = 'none';
+            }
+        }        
+});
+
